3cc4cc70302ca4cde707a2dbb5bea6c077b525f3
Duy Phuong Nguyen <maconline@Phuongs-Mac.local>
2020-08-14 17:55:39 +0700

    fix

--- download_api/master.js
+++ download_api/master.js
@@ -2,7 +2,7 @@ const cluster = require('cluster');
 const os = require('os');
 
 if (cluster.isMaster) {
-    const cpus = os.cpus().length - 4;
+    const cpus = os.cpus().length;
     console.log(`Forking for ${cpus} CPUs`);
     for (let i = 0; i < cpus; i++) {
         cluster.fork();
